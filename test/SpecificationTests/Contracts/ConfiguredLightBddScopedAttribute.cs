b23f5f7bde6180c4b047b108648dd1c22a2d051d
Diego Martin <martind@vitessepsp.com>
2024-10-31 13:22:11 +0100

    Fixed dependencies to make CI/CD work

--- test/SpecificationTests/Contracts/ConfiguredLightBddScopedAttribute.cs
+++
@@ -1,8 +0,0 @@
-using LightBDD.XUnit2;
-using SpecificationTests.Contracts;
-
-[assembly: ConfiguredLightBddScope] // the configured scope is applied on assembly
-namespace SpecificationTests.Contracts;
-
-internal class ConfiguredLightBddScopeAttribute 
-    : LightBddScopeAttribute;
